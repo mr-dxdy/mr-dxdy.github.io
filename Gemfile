3332112fddb077a725ec67564a56aceb909d5638
German Antsiferov <dxdy@bk.ru>
2015-03-03 17:30:12 +0300

    Begin css-style. Version 1

--- Gemfile
+++ Gemfile
@@ -6,6 +6,13 @@ ruby '2.2.0'
 # core
 gem "middleman", "~> 3.3.10"
 gem "middleman-blog", "~> 3.5.3"
+gem 'middleman-minify-html', '>= 3.4.0'
+
+# css
+gem 'sass'
+
+# locale
+gem 'russian', '~> 0.6.0'
 
 # For feed.xml.builder
 gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -2,31 +2,32 @@
 # Blog settings
 ###
 
-# Time.zone = "UTC"
+# time
+Time.zone = "Moscow"
+
+# locale
+activate :i18n, :langs => [:en]
+Russian.init_i18n
 
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
-  # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
+  blog.prefix = "posts"
+  blog.sources = ":year/:month-:day-:title.html"
+  blog.permalink = ":year/:month/:day/:title.html"
+  blog.taglink = "categories/:tag.html"
+  blog.layout = "layout.html"
+  blog.summary_separator = /(READMORE)/
+  blog.summary_length = 250
+  blog.year_link = ":year.html"
+  blog.month_link = ":year/:month.html"
+  blog.day_link = ":year/:month/:day.html"
+  blog.default_extension = ".markdown"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
 
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/:num"
 end
 
 page "/feed.xml", layout: false
@@ -79,18 +80,17 @@ page "/feed.xml", layout: false
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
+set :encoding, 'utf-8'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
