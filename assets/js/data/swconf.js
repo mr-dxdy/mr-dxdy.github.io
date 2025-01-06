287759808f9f0959708761beb2674cb538deb579
German Antsiferov <dxdy@bk.ru>
2025-01-06 21:36:07 +0300

    Automated commit at 2025-01-06 21:36:07

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1736188567',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/tags/', '/archives/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
